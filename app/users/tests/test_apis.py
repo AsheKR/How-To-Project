d5cea37df11369f262825ecfd36da30921c6f4b1
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-09 04:31:08 -0800

    Test: 기본 유저 생성 API 테스트코드 작성

---
+++ app/users/tests/test_apis.py
@@ -0,0 +1,23 @@
+from django.shortcuts import resolve_url
+
+
+class TestUserModel:
+
+    @staticmethod
+    def _create_users(client, dump):
+        context = {
+            'user_id': dump,
+            'password': 'asd',
+            'email': dump+'@'+dump+'.com',
+            'nickname': dump,
+        }
+
+        response = client.post(resolve_url('users:user_create'),
+                               data=context)
+        return response
+
+    def test_create_user_api(self, client):
+        response = self._create_members(client, 'asd')
+
+        assert response.status_code == 201, 'Create User API Status Code not 201'
+        assert response.json()['token'], 'Create User API no Token Returned'
