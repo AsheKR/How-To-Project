c7f315713be2dbf8dae16d30acdb02fbf358b7a2
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-09 04:36:23 -0800

    Test: UserAPI class 이름 변경

--- app/users/tests/test_apis.py
+++ app/users/tests/test_apis.py
@@ -1,7 +1,7 @@
 from django.shortcuts import resolve_url
 
 
-class TestUserModel:
+class TestUserAPI:
 
     @staticmethod
     def _create_users(client, dump):
@@ -17,7 +17,7 @@ class TestUserModel:
         return response
 
     def test_create_user_api(self, client):
-        response = self._create_members(client, 'asd')
+        response = self._create_users(client, 'asd')
 
         assert response.status_code == 201, 'Create User API Status Code not 201'
         assert response.json()['token'], 'Create User API no Token Returned'
