0070f0654c4eae30d4e807f65976013cb7d4bf55
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-09 03:10:32 -0800

    Feat: Django Project 기본 설정

---
+++ app/config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEV_JSON = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+WSGI_APPLICATION = 'config.wsgi.dev.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = DEV_JSON['DATABASES']
---
+++ app/config/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+PRODUCTION_JSON = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'config.wsgi.production.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = PRODUCTION_JSON['DATABASES']
