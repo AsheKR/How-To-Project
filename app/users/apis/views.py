1bb4b3087454da2e614aae32f6c23c9460edb8bc
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-12 00:13:54 -0800

    Refactor: follower, following list를 가져오는데 user_id를 사용함

--- app/users/apis/views.py
+++ app/users/apis/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
 from rest_framework import generics, status, permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -56,6 +57,8 @@ class UserProfileRetrieveUpdateDestroyGenericAPIView(generics.RetrieveUpdateDest
 class UserFollowingCreateListAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
+        if not kwargs.get('user_id'):
+            kwargs['user_id'] = request.user.user_id
         queryset = User.objects.filter(from_user_relation__to_user__user_id=kwargs.get('user_id'),
                                        from_user_relation__deleted_at=None,
                                        )
@@ -63,6 +66,8 @@ class UserFollowingCreateListAPIView(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            raise PermissionDenied({'detail': '로그인된 유저가 아닙니다.'})
         obj, created = UserRelation.objects.get_or_create(
             from_user=request.user,
             to_user=get_object_or_404(User.objects.filter(deleted_at=None), user_id=kwargs.get('user_id')),
@@ -84,6 +89,9 @@ class UserFollowingCreateListAPIView(APIView):
 class UserFollowerListAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
+        if not kwargs.get('user_id'):
+            kwargs['user_id'] = request.user.user_id
+
         queryset = User.objects.filter(to_user_relation__from_user__user_id=kwargs.get('user_id'),
                                        to_user_relation__deleted_at=None,
                                        )
--- app/users/urls.py
+++ app/users/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', views.UserCreateGenericAPIView.as_view(), name='create'),
     path('me/', views.UserProfileRetrieveUpdateDestroyGenericAPIView.as_view(), name='me-profile'),
     path('login/', views.UserLoginView.as_view(), name='login'),
+    path('following/me/', views.UserFollowingCreateListAPIView.as_view(), name='me-following'),
     path('following/<str:user_id>/', views.UserFollowingCreateListAPIView.as_view(), name='following'),
+    path('follower/me/', views.UserFollowerListAPIView.as_view(), name='me-follower'),
     path('follower/<str:user_id>/', views.UserFollowerListAPIView.as_view(), name='follower'),
     path('<str:user_id>/', views.UserProfileRetrieveGenericAPIView.as_view(), name='profile'),
 ]
