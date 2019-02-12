98d4b1edb95b8f41347e7b08a5178877552af604
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-12 05:29:58 -0800

    Test: Post CRUD API 테스트

--- app/posts/tests/test_apis.py
+++ app/posts/tests/test_apis.py
@@ -23,7 +23,7 @@ class TestPostAPI(BaseTestMixin):
     def setup_method(self, method):
         self._create_post_category()
 
-    def test_create_post_api(self, client):
+    def test_list_create_post_api(self, client):
         response = self._create_users(client, 'asd')
 
         header = {
@@ -37,9 +37,63 @@ class TestPostAPI(BaseTestMixin):
         }
 
         response = client.post(
-            resolve_url('posts:create'),
+            resolve_url('posts:list_create'),
             **header,
             data=context,
         )
 
         assert response.status_code == 201
+
+        response = client.get(
+            resolve_url('posts:list_create'),
+        )
+
+        assert response.status_code == 200
+        assert len(response.json()) == 1
+
+    def test_retrieve_update_destroy_post_api(self, client):
+        response = self._create_users(client, 'asd')
+
+        header = {
+            'HTTP_AUTHORIZATION': 'Token ' + response.json()['token']
+        }
+
+        context = {
+            'category': 'category1',
+            'title': 'title1',
+            'content': 'content',
+        }
+
+        _ = client.post(
+            resolve_url('posts:list_create'),
+            **header,
+            data=context,
+        )
+
+        context = {
+            'category': 'category2',
+            'title': 'title2',
+            'content': 'content2',
+        }
+
+        response = client.get(
+            resolve_url('posts:retrieve_update_destroy', pk=1),
+        )
+
+        assert response.status_code == 200
+
+        response = client.patch(
+            resolve_url('posts:retrieve_update_destroy', pk=1),
+            **header,
+            data=context,
+            content_type='application/json'
+        )
+
+        assert response.json()['title'] == 'title2'
+        assert response.json()['content'] == 'content2'
+
+        response = client.delete(
+            resolve_url('posts:retrieve_update_destroy', pk=1),
+        )
+
+        assert response.status_code == 204
