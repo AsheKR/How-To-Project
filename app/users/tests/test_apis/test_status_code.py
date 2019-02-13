ea06f8336fc4093251214f9957c31a5583baf22c
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-13 03:38:10 -0800

    Test: user_id는 -,_ 이외에 특수문자를 허용하지 않는 테스트 작성

--- app/users/tests/test_apis/test_status_code.py
+++ app/users/tests/test_apis/test_status_code.py
@@ -59,3 +59,25 @@ class TestUserStatusCodeAPI(BaseTestMixin, BaseTestUserContext):
 
         assert json['errors'][0]['code'] == '2041'
         assert json['errors'][0]['field'] == 'user_id'
+
+    def test_create_user_user_id_not_allow_uppercase(self, client):
+        context = self._get_context('A0lowercase')
+
+        response = self._create_users_with_context(client, context)
+
+        assert response.status_code == 400
+        json = response.json()
+
+        assert json['errors'][0]['code'] == '2013'
+        assert json['errors'][0]['field'] == 'user_id'
+
+    def test_create_user_user_id_not_allow_special_character_except_hypen_and_underline(self, client):
+        context = self._get_context('[d(-_-)b]')
+
+        response = self._create_users_with_context(client, context)
+
+        assert response.status_code == 400
+        json = response.json()
+
+        assert json['errors'][0]['code'] == '2013'
+        assert json['errors'][0]['field'] == 'user_id'
