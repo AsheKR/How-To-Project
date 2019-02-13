b663cacdcfaaf436ede002b9ccb4316c89585736
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-13 02:59:34 -0800

    Test: user_id는 숫자로 시작하는 것을 허용하지 않는다.

--- app/users/tests/test_apis/test_status_code.py
+++ app/users/tests/test_apis/test_status_code.py
@@ -19,3 +19,19 @@ class TestUserStatusCodeAPI(BaseTestMixin):
         for i, key in enumerate(tuple(context)):
             assert json['errors'][i]['code'] == '2031'
             assert json['errors'][i]['field'] == key
+
+    def test_create_user_user_id_start_with_numbers_occur_400(self, client):
+        context = {
+            'user_id': '1asdf11',
+            'password': 'asd',
+            'email': 'asd@asd.com',
+            'nickname': 'asd',
+        }
+
+        response = self._create_users_with_context(client, context)
+
+        assert response.status_code == 400
+        json = response.json()
+
+        assert json['errors'][0]['code'] == 'US01'
+        assert json['errors'][0]['field'] == 'user_id'
