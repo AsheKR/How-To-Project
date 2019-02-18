135e826eca87c9d5ff2ea20ff5d6bdf32400e095
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-18 00:15:35 -0800

    Refactor: Post Like에 대한 Validation 작성

--- app/posts/apis/views.py
+++ app/posts/apis/views.py
@@ -5,6 +5,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from base.base_error_mixins import NotRequireSerializerFriendlyErrorMessagesMixin
 from posts.apis.filters import PostFilter
 from posts.apis.permissions import PostUpdateDestroyMustBeOwner
 from posts.apis.serializers import PostCategorySerializer, PostSerializer
@@ -36,12 +37,16 @@ class PostRetrieveUpdateDestroyGenericAPIView(generics.RetrieveUpdateDestroyAPIV
     )
 
 
-class PostLikeToggleAPIView(APIView):
+class PostLikeToggleAPIView(NotRequireSerializerFriendlyErrorMessagesMixin, APIView):
     permission_classes = (
         permissions.IsAuthenticated,
     )
 
     def post(self, request, *args, **kwargs):
-        post = get_object_or_404(Post.objects.all(), pk=kwargs.get('pk'))
+        post = get_object_or_404(Post.objects.filter(deleted_at=None), pk=kwargs.get('pk'))
+        if post.author == request.user:
+            self.register_error_403(error_message='자신의 포스트에는 좋아요를 누를 수 없습니다.',
+                                    error_code='1101',
+                                    field_name='like')
         status = post.like_toggle(request.user)
         return Response(status=status)
