961e9710e4b8edc8a7872ff939e6fc30e6da8e2f
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-20 00:05:38 -0800

    [Refactor] post comment reply mention 기능 추가

--- app/posts/apis/serializers.py
+++ app/posts/apis/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.db.models import F
 from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 from rest_framework_friendly_errors.mixins import FriendlyErrorMessagesMixin
@@ -42,18 +44,30 @@ class PostCommentSerializer(FriendlyErrorMessagesMixin, serializers.ModelSeriali
             'post',
             'author',
             'parent',
+            'mention',
             'content',
         )
         read_only_fields = (
             'post',
             'author',
             'parent',
+            'mention',
         )
 
     def create(self, validated_data):
         author = self.context.get('request').user
         post = get_object_or_404(Post.objects.filter(deleted_at=None),
                                  pk=self.context.get('view').kwargs.get('pk'))
+
+        if self.context.get('request').data.get('parent'):
+            parent = get_object_or_404(PostComment.objects.filter(deleted_at=None),
+                                       pk=self.context.get('request').data.get('parent'))
+            validated_data['parent'] = parent
+
+        if self.context.get('request').data.get('mention'):
+            mention = get_object_or_404(get_user_model().objects.filter(deleted_at=None),
+                                        pk=self.context.get('request').data.get('mention'))
+            validated_data['mention'] = mention
 
         instance = PostComment.objects.create(
             author=author,
